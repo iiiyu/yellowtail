99a385d81911ce4113aabb1f3a071a2a8ce89f4c
Ewan Xiao <apple.iiiyu@gmail.com>
2023-02-06 23:57:40 +1300

    try decode eth_call result

--- apps/toolkit/lib/helper.ex
+++ apps/toolkit/lib/helper.ex
@@ -9,4 +9,13 @@ defmodule Toolkit.Helper do
   def to_address(hex_string) do
     hex_string |> remove_zero() |> then(&("0x" <> &1))
   end
+
+  def to_hex_list(hex_string) do
+    data = hex_string
+    sub_string_length = 32
+
+    for i <- 0..(div(String.length(data), sub_string_length) - 1) do
+      String.slice(data, i * sub_string_length, sub_string_length)
+    end
+  end
 end
--- apps/toolkit/lib/toolkit.ex
+++ apps/toolkit/lib/toolkit.ex
@@ -49,7 +49,7 @@ defmodule Toolkit do
 
     pair_contract_address =
       pair_contract_address_hex
-      |> Help.to_address()
+      |> Helper.to_address()
 
     abi_token_0_encoded_data = ABI.encode("token0()", []) |> Base.encode16(case: :lower)
     abi_token_1_encoded_data = ABI.encode("token1()", []) |> Base.encode16(case: :lower)
@@ -57,27 +57,79 @@ defmodule Toolkit do
     {:ok, token_0_address_hex} =
       HttpClient.eth_call(%{data: "0x" <> abi_token_0_encoded_data, to: pair_contract_address})
 
-    token_0_address = token_0_address_hex |> Help.to_address()
+    token_0_address = token_0_address_hex |> Helper.to_address()
 
     {:ok, token_1_address_hex} =
       HttpClient.eth_call(%{data: "0x" <> abi_token_1_encoded_data, to: pair_contract_address})
 
-    token_1_address = token_1_address_hex |> Help.to_address()
+    token_1_address = token_1_address_hex |> Helper.to_address()
 
     abi_name_encoded_data = ABI.encode("name()", []) |> Base.encode16(case: :lower)
     abi_symbol_encoded_data = ABI.encode("symbol()", []) |> Base.encode16(case: :lower)
     abi_total_supply_encoded_data = ABI.encode("totalSupply()", []) |> Base.encode16(case: :lower)
 
-    {:ok, token_0_name} =
+    {:ok, token_0_name_hex} =
       HttpClient.eth_call(%{data: "0x" <> abi_name_encoded_data, to: token_0_address})
 
-    data = token_0_name |> String.slice(2..-1)
-    sub_string_length = 32
+    {:ok, token_0_symbol_hex} =
+      HttpClient.eth_call(%{data: "0x" <> abi_symbol_encoded_data, to: token_0_address})
 
-    for i <- 0..(div(String.length(data), sub_string_length) - 1) do
-      String.slice(data, i * sub_string_length, sub_string_length)
-      |> Hexate.decode_to_list()
-      |> Kernel.inspect()
-    end
+    {:ok, token_0_total_supply_hex} =
+      HttpClient.eth_call(%{data: "0x" <> abi_total_supply_encoded_data, to: token_0_address})
+
+    # token_0_symbol_hex
+    # |> String.slice(2..-1)
+    # |> Base.decode16!(case: :lower)
+    # |> TypeDecoder.decode_raw([{:uint, 256}])
+    # |> List.first()
+
+    token_0_name =
+      token_0_name_hex
+      |> String.slice(2..-1)
+      |> Base.decode16!(case: :lower)
+      |> ABI.TypeDecoder.decode(%ABI.FunctionSelector{
+        function: nil,
+        types: [
+          :string
+        ]
+      })
+
+    token_0_symbol =
+      token_0_symbol_hex
+      |> String.slice(2..-1)
+      |> Base.decode16!(case: :lower)
+      |> ABI.TypeDecoder.decode(%ABI.FunctionSelector{
+        function: nil,
+        types: [
+          :string
+        ]
+      })
+
+    token_0_total_supply =
+      token_0_total_supply_hex
+      |> String.slice(2..-1)
+      |> Base.decode16!(case: :lower)
+      |> ABI.TypeDecoder.decode(%ABI.FunctionSelector{
+        function: nil,
+        types: [
+          {:uint, 256}
+        ]
+      })
+
+    {{:token0, token_0_name, token_0_symbol, token_0_total_supply}, {:token1}}
+
+    # ABI.decode("(string)", data)
+
+    # |> Helper.to_hex_list()
+    # |> List.flatten()
+    # |> Enum.map(fn i -> Hexate.decode_to_list(i) end)
+
+    # sub_string_length = 32
+
+    # for i <- 0..(div(String.length(data), sub_string_length) - 1) do
+    #   String.slice(data, i * sub_string_length, sub_string_length)
+    #   |> Hexate.decode_to_list()
+    #   |> Kernel.inspect()
+    # end
   end
 end
